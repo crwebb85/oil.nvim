d695a7b53dc5da6953eb660f8a0b5956660adb5d
Chris <crwebb85@gmail.com>
2025-11-15 11:49:20 -0500

    feat(test): Added Windows support for running tests

--- tests/url_spec.lua
+++ tests/url_spec.lua
@@ -4,39 +4,49 @@ local util = require("oil.util")
 local uv = vim.uv or vim.loop
 
 describe("url", function()
-  if not uv.os_uname().version:match("Windows") then
-    --These tests rely on functions like vim.fn.getcwd and vim.loop.os_homedir
-    --returning unix style paths so these test don't make sense to run on Windows
-    it("get parent url for empty string path", function()
-      local input = ""
-      local expected = "oil://" .. util.addslash(vim.fn.getcwd())
+  it("get parent url for empty string path", function()
+    if uv.os_uname().version:match("Windows") then
+      pending("Skipping this test on Windows because it relies on a unix styled current directory")
+      return
+    end
+    local input = ""
+    local expected = "oil://" .. util.addslash(vim.fn.getcwd())
 
-      local output, basename = oil.get_buffer_parent_url(input, true)
-      assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
-      assert.equals(nil, basename, string.format('Basename for path "%s" failed', input))
-    end)
+    local output, basename = oil.get_buffer_parent_url(input, true)
+    assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
+    assert.equals(nil, basename, string.format('Basename for path "%s" failed', input))
+  end)
 
-    it("get parent url for term name", function()
-      local input = "term://~/oil.nvim//52953:/bin/sh"
-      local expected = "oil://" .. vim.loop.os_homedir() .. "/oil.nvim/"
-      local output, basename = oil.get_buffer_parent_url(input, true)
-      assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
-      assert.equals(nil, basename, string.format('Basename for path "%s" failed', input))
-    end)
-
-    it("get parent url for unix path", function()
-      local input = "/foo/bar.txt"
-      local expected = "oil:///foo/"
-      local expected_basename = "bar.txt"
-      local output, basename = oil.get_buffer_parent_url(input, true)
-      assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
-      assert.equals(
-        expected_basename,
-        basename,
-        string.format('Basename for path "%s" failed', input)
+  it("get parent url for term name", function()
+    if uv.os_uname().version:match("Windows") then
+      pending(
+        "Skipping this test on Windows because it relies on a unix styled current home directory"
       )
-    end)
-  end
+      return
+    end
+    local input = "term://~/oil.nvim//52953:/bin/sh"
+    local expected = "oil://" .. vim.loop.os_homedir() .. "/oil.nvim/"
+    local output, basename = oil.get_buffer_parent_url(input, true)
+    assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
+    assert.equals(nil, basename, string.format('Basename for path "%s" failed', input))
+  end)
+
+  it("get parent url for unix path", function()
+    if uv.os_uname().version:match("Windows") then
+      pending("Skipping this test on Windows because it relies on a unix styled absolute path")
+      return
+    end
+    local input = "/foo/bar.txt"
+    local expected = "oil:///foo/"
+    local expected_basename = "bar.txt"
+    local output, basename = oil.get_buffer_parent_url(input, true)
+    assert.equals(expected, output, string.format('Parent url for path "%s" failed', input))
+    assert.equals(
+      expected_basename,
+      basename,
+      string.format('Basename for path "%s" failed', input)
+    )
+  end)
 
   it("get parent url for oil path", function()
     local input = "oil:///foo/bar.txt"
